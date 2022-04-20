da12a165a8944eb67cd412f7cefe124e11c91db5
Jeon Eunse <eunseo.jeon1110@gmail.com>
2022-04-20 19:18:01 +0900

    CSV 읽기 완

--- Assets/Script/CSVParse.cs
+++ Assets/Script/CSVParse.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
+
 public static class CSVParse
 {
     public static List<List<string>> Parse(TextAsset text)
@@ -10,7 +11,9 @@ public static class CSVParse
         TextReader reader = stringReader;
 
         List<List<string>> result = new List<List<string>>();       // 결과 리스트
+
         bool isQuote = false;          // "확인
+        bool isOneQuote = false;    // "를 1번이라도 만나면 확인
 
         string line;        // 읽은 라인
         List<string> tempLine = new List<string>(); // 결과 리스트에 담길 리스트
@@ -23,10 +26,34 @@ public static class CSVParse
                 switch (ch)
                 {
                     case ',':
-                        tempLine.Add(tempStr);
-                        tempStr = "";
+                        if (isQuote)
+                        {
+                            tempStr += ch;
+                        }
+                        else
+                        {
+                            // 앞의 "삭제
+                            if (isOneQuote)
+                            {
+                                tempStr = tempStr.Remove(tempStr.Length - 1);
+                                isOneQuote = false;
+                            }
+
+                            tempLine.Add(tempStr);
+                            tempStr = "";
+                        }
                         break;
                     case '"':
+                        if (isQuote)
+                        {
+                            tempStr += ch;
+                            isQuote = false;
+                        }
+                        else
+                        {
+                            isOneQuote = true;
+                            isQuote = true;
+                        }
                         break;
                     default:
                         tempStr += ch;
@@ -34,9 +61,24 @@ public static class CSVParse
                 }
             }
 
-            // 다음 라인
-            result.Add(tempLine);
-            tempLine = new List<string>();
+            if (!isQuote)
+            {
+                // 다음 라인
+                // 앞의 "삭제
+                if (isOneQuote)
+                {
+                    tempStr = tempStr.Remove(tempStr.Length - 1);
+                    isOneQuote = false;
+                }
+                tempLine.Add(tempStr);
+                result.Add(tempLine);
+                tempLine = new List<string>();
+                tempStr = "";
+            }
+            else
+            {
+                tempStr += "\n";
+            }
         }
 
         return result;
--- Assets/Script/TestCSV.cs
+++ Assets/Script/TestCSV.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class TestList
+{
+    public List<string> Test;
+}
+
 public class TestCSV : MonoBehaviour
 {
     public TextAsset text;
-    public List<List<string>> tempList;
+    public List<TestList> tempShowList;
 
     void Start()
     {
-        tempList = CSVParse.Parse(text);
-
+        List<List<string>> temp= CSVParse.Parse(text);
+        foreach (List<string> item in temp)
+        {
+            TestList testList = new TestList();
+            testList.Test = item;
+            tempShowList.Add(testList);
+        }
     }
 
     void Update()
